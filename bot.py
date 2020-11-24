2e0194bd616701df76fc5929425ceaed11fbaa89
Carlos Chacon <carlos26ch@gmail.com>
2020-11-23 20:24:29 -0400

    ImageUpload

--- bot.py
+++ bot.py
@@ -1,15 +1,27 @@
 import discord
 import os
-#import twint_config.py
 from discord.ext import commands
+import io
+import aiohttp
 
-client = commands.Bot(command_prefix = "$")
-
+client = commands.Bot(command_prefix = "$")  
+  
 @client.event
 async def on_ready():
-    print("Bot is ready")
+    print("===== Bot is ready =====")
+    print(f"Latency {round(client.latency * 1000)}ms")
 
 @client.command()
 async def ping(ctx):
     await ctx.send(f"Pong! --> {round(client.latency * 1000)}ms")
+
+@client.command()
+async def precio(ctx):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(https://monitordolarweb.com/gen_imagen/mdw.jpg) as resp:
+            if resp.status != 200:
+                return await channel.send('Could not download file...')
+            data = io.BytesIO(await resp.read())
+            await channel.send(file=discord.File(data, 'mdw.jpg'))
+
 client.run(os.environ.get("token"))
