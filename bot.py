8d2176ad20375898c91ffd79d7fdd63737135fc3
Carlos Chacon <carlos26ch@gmail.com>
2020-12-17 20:59:52 -0400

    test hello word

--- bot.py
+++ bot.py
@@ -4,12 +4,13 @@ from discord.ext import commands
 import io
 import aiohttp
 
-client = commands.Bot(command_prefix = "$")  
+client = commands.Bot(command_prefix = "$")
 
 @client.event
 async def on_ready():
     print("===== Bot is ready =====")
     print(f"Latency {round(client.latency * 1000)}ms")
+    print("Hello world")
 
 @client.command()
 async def ping(ctx):
