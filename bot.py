c0384053c7aadb7914652b2156297e4205cc9536
Carlos Chacon <carlos26ch@gmail.com>
2021-01-05 23:42:51 -0400

    added Zenquote

--- bot.py
+++ bot.py
@@ -3,18 +3,31 @@ import os
 from discord.ext import commands
 import io
 import aiohttp
+import requests
+import json
 
 client = commands.Bot(command_prefix = "%")
+def get_quote():
+    response = requests.get('https://zenquotes.io/api/random')
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + ' -' +  json_data[0]['a']
+    return quote
 
 @client.event
 async def on_ready():
     print("===== Bot is ready =====")
+    print(f'We have logged in as {client.user}')
     print(f"Latency {round(client.latency * 1000)}ms")
-    print("Hello world")
+    print("Hello world i'm alive")
 
 @client.command()
 async def ping(ctx):
     await ctx.send(f"Pong! --> {round(client.latency * 1000)}ms")
+
+@client.command():
+async def saludo(ctx):
+    quote = get_quote()
+    await ctx.send(quote)
 
 @client.command()
 async def precio(ctx):
