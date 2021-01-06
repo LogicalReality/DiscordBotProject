4d3e53f6da199e0c624a2a4477201d674332a3a9
Carlos Chacon <carlos26ch@gmail.com>
2021-01-06 00:10:45 -0400

    Zenquote aiohttp

--- bot.py
+++ bot.py
@@ -3,15 +3,9 @@ import os
 from discord.ext import commands
 import io
 import aiohttp
-import requests
 import json
 
 client = commands.Bot(command_prefix = "%")
-def get_quote():
-    response = requests.get('https://zenquotes.io/api/random')
-    json_data = json.loads(response.text)
-    quote = json_data[0]['q'] + ' -' +  json_data[0]['a']
-    return quote
 
 @client.event
 async def on_ready():
@@ -25,11 +19,6 @@ async def ping(ctx):
     await ctx.send(f"Pong! --> {round(client.latency * 1000)}ms")
 
 @client.command()
-async def saludo(ctx):
-    quote = get_quote()
-    await ctx.send(quote)
-
-@client.command()
 async def precio(ctx):
     async with aiohttp.ClientSession() as session:
         async with session.get("https://monitordolarweb.com/gen_imagen/mdw.jpg") as resp:
@@ -37,5 +26,10 @@ async def precio(ctx):
                 return await ctx.send('Could not download file...')
             data = io.BytesIO(await resp.read())
             await ctx.send(file=discord.File(data, 'mdw.jpg'))
+
+        async with session.get('https://zenquotes.io/api/random') as quote:
+            json_data = json.loads(quote.text)
+            quote = json_data[0]['q'] + ' -' + json_data[0]['a']
+            await  ctx.send(quote)
 
 client.run(os.environ.get("token"))
